a9af7b1e4185b371256fbe3864103f4a2e613a07
Youcef Houssem Eddine Bousmaha <ky_bousmaha@esi.dz>
2022-03-22 09:27:09 +0300

    adding the CartListItems ListView and general styling.

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -23,13 +23,13 @@ class CartScreen extends StatelessWidget {
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   const Text(
-                    'Total',
+                    'total',
                     style: TextStyle(fontSize: 20),
                   ),
                   const Spacer(),
                   Chip(
                     label: Text(
-                      '\$${cart.totalPrice}',
+                      '\$${cart.totalPrice.toStringAsFixed(2)}',
                       style: TextStyle(color: Theme.of(context).primaryTextTheme.headline6?.color),
                     ),
                     backgroundColor: Theme.of(context).primaryColor,
@@ -42,7 +42,51 @@ class CartScreen extends StatelessWidget {
               ),
             ),
           ),
+          const SizedBox(height: 10),
+          Expanded(
+            child: ListView.builder(
+              itemCount: cart.items.length,
+              itemBuilder: (_, index) => CartListItem(
+                cart.items.values.toList()[index],
+              ),
+            ),
+          ),
         ],
+      ),
+    );
+  }
+}
+
+class CartListItem extends StatelessWidget {
+  const CartListItem(
+    this.cart, {
+    Key? key,
+  }) : super(key: key);
+
+  final CartItem cart;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: const EdgeInsets.symmetric(horizontal: 15, vertical: 4),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: ListTile(
+          leading: CircleAvatar(
+            child: FittedBox(
+              child: Text(
+                '\$${cart.price.toStringAsFixed(2)}',
+                style: const TextStyle(fontSize: 10),
+              ),
+            ),
+          ),
+          title: Text(cart.title),
+          subtitle: Text('Total: \$${(cart.price * cart.quantity).toStringAsFixed(2)}'),
+          trailing: Text(
+            'x${cart.quantity}',
+            style: const TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+          ),
+        ),
       ),
     );
   }
